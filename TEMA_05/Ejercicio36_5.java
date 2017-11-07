ed8e6857caa656540872f1f02f4525ac1ad8db4b
lucia-blanco <lublasal@gmail.com>
2017-11-07 08:40:44 +0100

    Nuevo

--- TEMA_05/Ejercicio36_5.java
+++ TEMA_05/Ejercicio36_5.java
@@ -8,16 +8,38 @@
   public static void main(String[] args) {
     System.out.print("Introduce un número: ");
     long numUs = Long.parseLong(System.console().readLine());
-    // Cuenta los números
-    int numBasura = numUs;
-    int numDig = 0;
+    boolean palindromo = false;
+    // Cuenta los números y calcula el reves
+    long numBasura = numUs;
+    long numDig = 0;
+    long numReves = 0;
     while (numBasura > 0){
+      numReves = ((numReves*10) + (numBasura % 10));
       numBasura /=10;
       numDig++;
     }
-    numBasura = numUs;
-    int i = 0;
-    while (i <= numDig/2){
+    // Los compara
+    long numDerecho = numUs;
+    long cifraDerecho = 0;
+    long cifraReves = 0;
+    numDig /= 2;
+    do{
+      cifraDerecho = numDerecho%10;
+      cifraReves = numReves%10;
+      if (cifraDerecho == cifraReves){
+        palindromo = true;
+      } else {
+        palindromo = false;
+      }
+      numDerecho /= 10;
+      numReves /= 10;
+      numDig--;
+    } while ((numDig > 0) && (palindromo == true));
+    System.out.print("El número "+numUs);
+    if (palindromo == true){
+      System.out.print(" es un palíndromo.");
+    } else {
+      System.out.print(" no es un palíndromo.");
     }
   }
 }
