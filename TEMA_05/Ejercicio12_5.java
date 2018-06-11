6668d9b0df1dc3a779618cc1caf68f8bd5005f18
Lucía Blanco <32088542+lucia-blanco@users.noreply.github.com>
2018-06-11 18:20:15 +0200

    corregido

--- TEMA_05/Ejercicio12_5.java
+++ TEMA_05/Ejercicio12_5.java
@@ -6,13 +6,16 @@
  
  public class Ejercicio12_5 { // Clase principal
   public static void main(String[] args) {
-    System.out.print("Introduce un número ");
-    int cifras = Integer.parseInt(System.console().readLine());
+    int cifras; 
+    do {
+      System.out.print("Introduce un número ");
+      cifras = Integer.parseInt(System.console().readLine());
+    } while (cifras <= 2);
     int num1 = 0;
     int num2 = 1;
     int fibo = 0;
     System.out.print("Las primeras "+cifras+" de la serie de Fibonacci son: "+ num1+" "+num2+ " ");
-    for (int i = 0; i < cifras; i++){
+    for (int i = 0; i < (cifras-2); i++){
       fibo = num1 + num2;
       System.out.print(" "+fibo+" ");
       num1 = num2;
